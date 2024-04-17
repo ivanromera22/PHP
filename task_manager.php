07bab06f96de42943bdbf129b015801951ee660b
ivanromera22 <165598978+ivanromera22@users.noreply.github.com>
2024-04-17 12:44:00 +0200

    v1 final

--- task_manager.php
+++ task_manager.php
@@ -12,7 +12,7 @@ $conn = mysqli_connect($servername, $username, $password, $db);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "";
+
 
 if (!CLI()) {
     echo "Aquest script és d'unic ús a CLI\n";
@@ -23,14 +23,14 @@ function CLI() {
     return php_sapi_name() === 'cli';
 }
 
-// AFEGIR TASCA
+// AFEGIR TASCA 
 function afegir_tasca($conn, $titol, $descripcio) {
     if (empty($titol) || empty($descripcio)) {
         echo "[Error de sintaxis] Inserta text dins dels camps.\n";
         return;
     }
 
-    $query = "INSERT INTO table_tasks (titol, descripcio) VALUES ('$titol', '$descripcio')";
+    $query = "INSERT INTO tasques (titol, descripcio) VALUES ('$titol', '$descripcio')";
     $exec = mysqli_query($conn, $query);
 
     if ($exec) {
@@ -48,7 +48,7 @@ function editar_tasca($conn, $titol) {
         return;
     }
 
-    $query = "UPDATE table_tasks SET estat = '1' WHERE titol = '$titol'";
+    $query = "UPDATE tasques SET estat = '1' WHERE titol = '$titol'";
     $exec = mysqli_query($conn, $query);
     if ($exec) {
         echo "Tasca editada amb èxit.\n";
@@ -57,23 +57,25 @@ function editar_tasca($conn, $titol) {
     }
 }
 
-// LLISTAR TASCA //
+// LLISTAR TASCA 
 function llistar_tasca($conn) {
 
-    $query = "SELECT id, titol, descripcio FROM table_tasks WHERE estat = 0";
+    $query = "SELECT id, titol, descripcio, estat FROM tasques WHERE estat = 0";
     $exec = mysqli_query($conn, $query);
 
-    if ($exec) {
+    $num_rows = mysqli_num_rows($exec);
+
+    if ($num_rows > 0) {
         echo "LListant tasques pendents:\n";
         while ($row = mysqli_fetch_assoc($exec)) {
-            echo "ID: " . $row["id"] . ", Titulo: " . $row["titol"] . ", Descripción: " . $row["descripcio"] . "\n";
+            echo "ID: " . $row["id"] . ", Títol: " . $row["titol"] . ", Descripció: " . $row["descripcio"] . ", Estat: " . $row["estat"] . "\n";
         }
     } else {
         echo "No hi ha tasques pendents.\n";
     }
 }
 
-// ELIMINAR TASCA /
+// ELIMINAR TASCA
 function eliminar_tasca($conn, $titol) {
 
     if (empty($titol)) {
@@ -81,7 +83,7 @@ function eliminar_tasca($conn, $titol) {
         return;
     }
 
-    $query = "DELETE FROM table_tasks WHERE id = '$titol'";
+    $query = "DELETE FROM tasques WHERE id = '$titol'";
     $exec = mysqli_query($conn, $query);
 
     if ($exec) {
@@ -95,9 +97,9 @@ function help() {
     echo "\n========================================================================\n";
     echo "Això es un gestor de tasques\n";
     echo "(Recorda que la sintaxis es molt important)\n";
-    echo "Per afegir: php task_manager add <titol> <descripció>\n";
-    echo "Per editar: php task_manager edit <titol> \n";
-    echo "Per eliminar: php task_manager delete <titol>\n";
+    echo "Per afegir: php task_manager add <títol> <descripció>\n";
+    echo "Per editar: php task_manager edit <títol> \n";
+    echo "Per eliminar: php task_manager delete <id>\n";
     echo "Per llistar tasques pendents: php task_manager list\n";
     echo "========================================================================\n\n";
 }
@@ -107,28 +109,30 @@ switch ($argv[1]) {
         if ($argc == 4) {
             afegir_tasca($conn, $argv[2], $argv[3]);
         } else {
-            echo "error : php task_manager.php add <titul> <descripció>";
+            echo "error : php task_manager.php add <titol> <descripció>\n"; 
         }
         break;
+        
     case "edit":
         if ($argc == 3) {
             editar_tasca($conn, $argv[2]);
         } else {
-            echo "error : php task_manager.php edit <titul>";
+            echo "error : php task_manager.php edit <titol>\n";
         }
         break;
+        
     case "delete":
         if ($argc == 3) {
             eliminar_tasca($conn, $argv[2]);
         } else {
-            echo "error : php task_manager.php delete <titul>";
+            echo "error : php task_manager.php delete <titol>\n";
         }
         break;
     case "list":
         if ($argc == 2) {
             llistar_tasca($conn);
         } else {
-            echo "error : php task_manager.php list";
+            echo "error : php task_manager.php list\n";
         }
         break;
     default:
